6cc59237c08cf8e61bfdbc6759a0e7d23a3f3111
thig7179 <thiagodosantos64@gmail.com>
2021-08-22 21:35:14 -0300

    loja

---
+++ resources/views/admin/produto/_lib.blade.php
@@ -0,0 +1,4 @@
+@push('scripts')
+<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>tinymce.init({ selector:'textarea' });</script>
+@endpush
